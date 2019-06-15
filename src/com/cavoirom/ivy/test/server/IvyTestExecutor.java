23e3552190ea27c3ba42dc3cf0f6cb58095a83d6
Cavoi Rom <cavoirom.wanted@yahoo.com>
2019-06-15 15:42:06 +0700

    Resolve class loader different when running tests

--- src/com/cavoirom/ivy/test/server/IvyTestExecutor.java
+++ src/com/cavoirom/ivy/test/server/IvyTestExecutor.java
@@ -1,16 +1,13 @@
 package com.cavoirom.ivy.test.server;
 
-import java.util.Objects;
-import java.util.Optional;
-
-import org.apache.commons.lang3.StringUtils;
-import org.junit.runner.JUnitCore;
-
 import ch.ivyteam.ivy.application.IProcessModel;
 import ch.ivyteam.ivy.environment.Ivy;
 import ch.ivyteam.ivy.project.IvyProjectNavigationUtil;
-
 import com.cavoirom.ivy.test.runner.TestResult;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Objects;
+import java.util.Optional;
+import org.apache.commons.lang3.StringUtils;
 
 @SuppressWarnings("restriction")
 public class IvyTestExecutor {
@@ -19,8 +16,8 @@ public class IvyTestExecutor {
     Objects.requireNonNull(moduleName);
     TestResult result = new TestResult();
     try {
-      ClassLoader testRunnerClassLoader = findTestRunnerClassLoader(moduleName);
-      Class<?> testClass = loadTestClass(testClassPath, testRunnerClassLoader);
+      ClassLoader testClassLoader = findTestClassLoader(moduleName);
+      Class<?> testClass = getClass(testClassPath, testClassLoader);
       result = runTestClass(testClass);
     } catch (Exception ex) {
       Ivy.log().error("Fail to run test class: " + testClassPath, ex);
@@ -28,7 +25,7 @@ public class IvyTestExecutor {
     return result;
   }
 
-	private ClassLoader findTestRunnerClassLoader(String moduleName) {
+  private ClassLoader findTestClassLoader(String moduleName) {
     return Optional.of(moduleName)
         .flatMap(
             item ->
@@ -40,16 +37,44 @@ public class IvyTestExecutor {
         .orElse(this.getClass().getClassLoader());
   }
 
-  private Class<?> loadTestClass(String testClassPath, ClassLoader testRunnerClassLoader)
+  private Class<?> getClass(String testClassPath, ClassLoader testClassLoader)
       throws ClassNotFoundException {
-    return Class.forName(testClassPath, true, testRunnerClassLoader);
+    return Class.forName(testClassPath, true, testClassLoader);
   }
 
-  private TestResult runTestClass(Class<?> testClass) {
-    JUnitCore junit = new JUnitCore();
-    TestResultListener testResultListener = new TestResultListener();
-    junit.addListener(testResultListener);
-    junit.run(testClass);
-    return testResultListener.getTestResult();
+  @SuppressWarnings("unchecked")
+  private TestResult runTestClass(Class<?> testClass)
+      throws IllegalAccessException, InstantiationException, ClassNotFoundException,
+          NoSuchMethodException, InvocationTargetException {
+    /*
+     * <code>
+     * JUnitCore junit = new JUnitCore();
+     * TestResultListener testResultListener = new TestResultListener();
+     * junit.addListener(testResultListener);
+     * junit.run(testClass);
+     * return testResultListener.getTestResult();
+     * </code>
+     *
+     * Because the class loaders are different, cause the Test annotation cannot be detected in
+     * test case. The code above need to be re-implemented in reflection.
+     *
+     */
+    ClassLoader testClassLoader = testClass.getClassLoader();
+    Class<Object> junitClass =
+        (Class<Object>) getClass("org.junit.runner.JUnitCore", testClassLoader);
+    Object junit = junitClass.newInstance();
+    Class<Object> testResultListenerClass =
+        (Class<Object>)
+            getClass("com.cavoirom.ivy.test.runner.TestResultListener", testClassLoader);
+    Object testResultListener = testResultListenerClass.newInstance();
+    junitClass
+        .getMethod(
+            "addListener", getClass("org.junit.runner.notification.RunListener", testClassLoader))
+        .invoke(junit, testResultListener);
+    junitClass.getMethod("run", Class[].class).invoke(junit, (Object) new Class[] {testClass});
+    Object testResult =
+        testResultListenerClass.getMethod("getTestResult").invoke(testResultListener);
+    String testResultJson = (String) testResult.getClass().getMethod("toJson").invoke(testResult);
+    return TestResult.fromJson(testResultJson);
   }
 }
